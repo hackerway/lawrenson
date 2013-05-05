82fe09fcef160e28e14aaa5c11e3ee49243dc016
Peter Hastie <pete@sillypog.com>
2013-05-05 16:46:53 -0700

    Initial rails folder with modified .gitignore.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lawrenson::Application.config.secret_token = 'c1d2af121f418426b92054b350d51d64048edc9bca1a51fb1deafebd6beefe76718126e4a204ee33299760ecf7f33fdfccec6ce010634b8082709c96f692218a'
