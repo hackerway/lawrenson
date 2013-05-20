83c212c96d6d564ed6f25eaf7c8e5f50b248a345
Peter Hastie <pete@sillypog.com>
2013-05-19 22:55:29 -0700

    Removed unused lines.

--- db/migrate/20130518034314_create_team_sport_foreign_key.rb
+++ db/migrate/20130518034314_create_team_sport_foreign_key.rb
@@ -1,11 +1,9 @@
 class CreateTeamSportForeignKey < ActiveRecord::Migration
   def up
-	#add_foreign_key(:teams, :sports, :name => 'team_sport_foreign_key')
 	add_foreign_key(:teams, :sports)
   end
 
   def down
   	remove_foreign_key(:teams, :sports)
-	#puts "Skipping foreign key rollback"
   end
 end
