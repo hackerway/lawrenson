1796c23bfd7479f675ed47b77b41f6d07a240749
Peter Hastie <pete@sillypog.com>
2013-05-21 22:10:33 -0700

    Added basic validation.

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -2,4 +2,7 @@ class Sport < ActiveRecord::Base
   attr_accessible :name
   
   has_many :team
+  
+  validates :name, :presence => true
+  validates :name, :uniqueness => true
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,7 @@ class Team < ActiveRecord::Base
   attr_accessible :logo_url, :name, :sport_id
   
   belongs_to :sport
+  
+  validates :name, :sport_id, :presence => true
+  validates :sport_id, :numericality => {:only_integer => true}
 end
