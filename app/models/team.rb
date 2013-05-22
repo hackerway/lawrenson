cdaffffd770a0389ddde04fcf0151abc6b875367
Peter Hastie <pete@sillypog.com>
2013-05-21 22:43:27 -0700

    Added check for format in url.

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,4 +5,8 @@ class Team < ActiveRecord::Base
   
   validates :name, :sport_id, :presence => true
   validates :sport_id, :numericality => {:only_integer => true}
+  validates :logo_url, :format => {
+	:with => %r{\.(gif|jpg|png)$}i,
+	:message => 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
